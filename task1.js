9058c9fde53cd94a4a479472116116ca3d59db20
Coreytaz <coreytaz321@gmail.com>
2022-07-31 20:27:08 +0500

    fix

--- task1.js
+++ task1.js
@@ -1,7 +1,7 @@
 function TableMultiple(num) {
     const arrayMultiplier = NumArray(num);
 
-    const numMultiple = "  " + arrayMultiplier.join(" ") + "\n";
+    const numMultiple = "   " + arrayMultiplier.join(" ") + "\n";
     const dashs = numMultiple.replace(/\d| |\W+n/g, "-");
     const tableNum = TableNumCreate(arrayMultiplier);
 
@@ -13,12 +13,12 @@ function TableMultiple(num) {
 }
 
 function NumArray(num) {
-    return Array.from({ length: num }, (e, i) => i + 1);
+    return Array(num).fill().map((e, i) => i + 1);
 }
 
 function TableNumCreate(arrayMultiplier) {
     const newArray = [];
-    for (let i = 1; i < arrayMultiplier.length; i++) {
+    for (let i = 1; i <= arrayMultiplier.length; i++) {
         newArray.push(
             i + "|" + arrayMultiplier.map((item) => item * i) + "\n"
         );
