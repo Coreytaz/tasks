17e4fba42d6417d51029a5d607f331b428384824
Coreytaz <coreytaz321@gmail.com>
2022-08-03 15:00:01 +0500

    fix

--- task1.js
+++ task1.js
@@ -1,14 +1,42 @@
 function TableMultiple(num) {
     const arrayMultiplier = NumArray(num);
-
-    const numMultiple = "   " + arrayMultiplier.join(" ") + "\n";
-    const dashs = numMultiple.replace(/\d| |\W+n/g, "-");
+    let numMultiple = [];
+    let lastArray = [];
     const tableNum = TableNumCreate(arrayMultiplier);
 
-    const finaliArray = [numMultiple, dashs, tableNum];
+    lastString = tableNum[tableNum.length - 1].map(item => item.toString().length);
+    firstColumnWidth = arrayMultiplier[arrayMultiplier.length - 1].toString().length
+    const rows = (arr, index) => {
+
+        let newArr = []
+
+        for (i = 0; i < arr.length; i++) {
+            newArr.push(arr[i].toString().padStart(lastString[i]))
+        }
+
+        lastArray.push(arrayMultiplier[index].toString().padStart(firstColumnWidth) + '|' + newArr.join(' ') + '\n')
+
+
+    }
+
+    for (let i = 0; i < tableNum.length; i++) {
+
+        rows(tableNum[i], i)
+    }
+
+    for (let i = 0; i < arrayMultiplier.length; i++) {
+
+        numMultiple.push(arrayMultiplier[i].toString().padStart(lastString[i]))
+    }
+
+    const dashs = numMultiple.join(" ").replace(/\d| |\W+n/g, "-");
+
+    let titleStringLength = numMultiple.join(" ").length + firstColumnWidth + 1
+
+    const finaliArray = [numMultiple.join(" ").padStart(titleStringLength) + '\n', dashs.padStart(titleStringLength) + '\n'];
 
     console.group("Задание 1 c таблицой умножения");
-    console.log(finaliArray.join(" "));
+    console.log(finaliArray.concat(lastArray).join(''));
     console.groupEnd();
 }
 
@@ -16,14 +44,14 @@ function NumArray(num) {
     return Array(num).fill().map((e, i) => i + 1);
 }
 
+
 function TableNumCreate(arrayMultiplier) {
     const newArray = [];
     for (let i = 1; i <= arrayMultiplier.length; i++) {
-        newArray.push(
-            i + "|" + arrayMultiplier.map((item) => item * i) + "\n"
-        );
+        newArray.push(arrayMultiplier.map((item) => item * i));
     }
-    return newArray.join(" ").replaceAll(",", " ");
+    return newArray;
 }
 
-TableMultiple(4);
+
+TableMultiple(10);
